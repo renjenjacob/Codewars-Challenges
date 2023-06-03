43084b13f79aa2c72df24f5a311a9e6ce52a9d12
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-06-02 21:26:33 -0400

    Update Sum-of-positive.js

--- 8kyu/Sum-of-positive.js
+++ 8kyu/Sum-of-positive.js
@@ -8,14 +8,14 @@
 //Solution 1
 
 function positiveSum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > 0) {
-        sum += arr[i];
-        }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
     }
-    return sum;
   }
+  return sum;
+}
 
 //Solution 2
 
