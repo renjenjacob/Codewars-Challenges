8f1682946fb60d83f0451802ad2be942fdca9e37
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-06-14 17:21:47 -0400

    Add files via upload

---
+++ 7kyu/Find-the-divisors!.js
@@ -0,0 +1,22 @@
+//Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime'.
+
+
+//Examples
+
+//divisors(12) --> [2,3,4,6]
+//divisors(25) --> [5]
+//divisors(13) --> "13 is prime"
+
+//Solution
+
+function divisors(integer) {
+    let divs = [];
+  
+    for(let i = 2; i < integer; i++) {
+        if(integer % i === 0) {
+    	    divs.push(i);
+        }
+    }
+  
+    return divs.length ? divs : integer + ' is prime';
+};
