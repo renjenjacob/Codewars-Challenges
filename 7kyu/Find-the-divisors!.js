e106dc5317a603871e62d75c6671b7e86ebd8247
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-06-14 17:25:02 -0400

    Update Find-the-divisors!.js

--- 7kyu/Find-the-divisors!.js
+++ 7kyu/Find-the-divisors!.js
@@ -1,5 +1,5 @@
-//Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime'.
-
+//Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest.
+// If the number is prime, return the string '(integer) is prime'.
 
 //Examples
 
