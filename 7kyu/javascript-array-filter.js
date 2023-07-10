758f70b733b9b60619c8959648ae8320a4e0a5f6
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-07-10 18:31:30 -0400

    Add files via upload

---
+++ 7kyu/javascript-array-filter.js
@@ -0,0 +1,11 @@
+//JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+//Example
+
+//getEvenNumbers([2,4,5,6]) --> [2,4,6]
+
+//Solution
+
+const getEvenNumbers = numbersArray => numbersArray.filter(num => num % 2 == 0)
+
+
